de7fcf4a06e1c92027cb7a3c91205283981b08eb
odzhan <odzhan@protonmail.com>
2018-10-18 18:52:38 +0100

    upd

--- os/linux/arm64/crypto/speck/spk128.s
+++ os/linux/arm64/crypto/speck/spk128.s
@@ -30,36 +30,36 @@
 # SPECK128/256 in ARM64 assembly
 # 80 bytes
 
-  .arch armv8-a  
-  .text
-  
-  .global speck128
+    .arch armv8-a  
+    .text
+    
+    .global speck128
 
-  // speck128(void*mk, void*data);
+    // speck128(void*mk, void*data);
 speck128:
-  // load 256-bit key
-  // k0 = k[0]; k1 = k[1]; k2 = k[2]; k3 = k[3];
-  ldp    x5, x6, [x0]
-  ldp    x7, x8, [x0, 16] 
-  // load 128-bit plain text
-  ldp    x2, x4, [x1]         // x0 = x[0]; x1 = k[1];
-  mov    x3, xzr              // i=0
+    // load 256-bit key
+    // k0 = k[0]; k1 = k[1]; k2 = k[2]; k3 = k[3];
+    ldp    x5, x6, [x0]
+    ldp    x7, x8, [x0, 16] 
+    // load 128-bit plain text
+    ldp    x2, x4, [x1]         // x0 = x[0]; x1 = k[1];
+    mov    x3, xzr              // i=0
 L0:
-  ror    x4, x4, 8
-  add    x4, x4, x2           // x1 = (R(x1, 8) + x0) ^ k0;
-  eor    x4, x4, x5           //
-  eor    x2, x4, x2, ror 61   // x0 = R(x0, 61) ^ x1;
-  mov    x9, x8               // backup k3
-  ror    x6, x6, 8
-  add    x8, x5, x6           // k3 = (R(k1, 8) + k0) ^ i;
-  eor    x8, x8, x3           //
-  eor    x5, x8, x5, ror 61   // k0 = R(k0, 61) ^ k3;
-  mov    x6, x7               // k1 = k2;
-  mov    x7, x9               // k2 = t;
-  add    x3, x3, 1            // i++;
-  cmp    x3, 34               // i < 34;
-  bne    L0 
-  
-  // save result
-  stp    x2, x4, [x1]         // x[0] = x0; x[1] = x1;
-  ret 
+    ror    x4, x4, 8
+    add    x4, x4, x2           // x1 = (R(x1, 8) + x0) ^ k0;
+    eor    x4, x4, x5           //
+    eor    x2, x4, x2, ror 61   // x0 = R(x0, 61) ^ x1;
+    mov    x9, x8               // backup k3
+    ror    x6, x6, 8
+    add    x8, x5, x6           // k3 = (R(k1, 8) + k0) ^ i;
+    eor    x8, x8, x3           //
+    eor    x5, x8, x5, ror 61   // k0 = R(k0, 61) ^ k3;
+    mov    x6, x7               // k1 = k2;
+    mov    x7, x9               // k2 = t;
+    add    x3, x3, 1            // i++;
+    cmp    x3, 34               // i < 34;
+    bne    L0 
+    
+    // save result
+    stp    x2, x4, [x1]         // x[0] = x0; x[1] = x1;
+    ret 
--- os/linux/arm64/crypto/speck/spk64.s
+++ os/linux/arm64/crypto/speck/spk64.s
@@ -30,36 +30,36 @@
 # SPECK64/128 in ARM64 assembly
 # 80 bytes
 
-  .arch armv8-a  
-  .text
-  
-  .global speck64
+    .arch armv8-a  
+    .text
+    
+    .global speck64
 
-  // speck64(void*mk, void*data);
+    // speck64(void*mk, void*data);
 speck64:
-  // load 128-bit key
-  // k0 = k[0]; k1 = k[1]; k2 = k[2]; k3 = k[3];
-  ldp    w5, w6, [x0]
-  ldp    w7, w8, [x0, 8] 
-  // load 64-bit plain text
-  ldp    w2, w4, [x1]         // x0 = x[0]; x1 = k[1];
-  mov    w3, wzr              // i=0
+    // load 128-bit key
+    // k0 = k[0]; k1 = k[1]; k2 = k[2]; k3 = k[3];
+    ldp    w5, w6, [x0]
+    ldp    w7, w8, [x0, 8] 
+    // load 64-bit plain text
+    ldp    w2, w4, [x1]         // x0 = x[0]; x1 = k[1];
+    mov    w3, wzr              // i=0
 L0:
-  ror    w2, w2, 8
-  add    w2, w2, w4           // x0 = (ROTR32(x0, 8) + x1) ^ k0;
-  eor    w2, w2, w5           //
-  eor    w4, w2, w4, ror 29   // x1 = ROTL32(x1, 3) ^ x0;
-  mov    w9, w8               // backup k3
-  ror    w6, w6, 8
-  add    w8, w5, w6           // k3 = (ROTR32(k1, 8) + k0) ^ i;
-  eor    w8, w8, w3           //
-  eor    w5, w8, w5, ror 29   // k0 = ROTL32(k0, 3) ^ k3;
-  mov    w6, w7               // k1 = k2;
-  mov    w7, w9               // k2 = t;
-  add    w3, w3, 1            // i++;
-  cmp    w3, 27               // i < 27;
-  bne    L0 
-  
-  // save result
-  stp    w2, w4, [x1]         // x[0] = x0; x[1] = x1;
-  ret 
+    ror    w2, w2, 8
+    add    w2, w2, w4           // x0 = (R(x0, 8) + x1) ^ k0;
+    eor    w2, w2, w5           //
+    eor    w4, w2, w4, ror 29   // x1 = R(x1, 3) ^ x0;
+    mov    w9, w8               // backup k3
+    ror    w6, w6, 8
+    add    w8, w5, w6           // k3 = (R(k1, 8) + k0) ^ i;
+    eor    w8, w8, w3           //
+    eor    w5, w8, w5, ror 29   // k0 = R(k0, 3) ^ k3;
+    mov    w6, w7               // k1 = k2;
+    mov    w7, w9               // k2 = t;
+    add    w3, w3, 1            // i++;
+    cmp    w3, 27               // i < 27;
+    bne    L0 
+    
+    // save result
+    stp    w2, w4, [x1]         // x[0] = x0; x[1] = x1;
+    ret 
