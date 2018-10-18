8e3ad41504dcfc285a5e6d2d94d3ff3efc07571f
odzhan <odzhan@protonmail.com>
2018-10-18 21:25:59 +0100

    upd

--- os/linux/arm64/crypto/cham/cham.c
+++ os/linux/arm64/crypto/cham/cham.c
@@ -32,16 +32,17 @@
 typedef unsigned int W;
 
 void cham(void*mk,void*p){
-  W rk[8],*x=p,*k=mk,i,t;
+  W rk[8],*w=p,*k=mk,i,t;
 
   F(4)
-    rk[i]=k[i]^R(k[i],31)^R(k[i],24),
-    rk[(i+4)^1]=k[i]^R(k[i],31)^R(k[i],21);
+    t=k[i]^R(k[i],31),
+    rk[i]=t^R(k[i],24),
+    rk[(i+4)^1]=t^R(k[i],21);
 
   F(80)
-    t=x[3],*x^=i,x[3]=rk[i&7],
-    x[3]^=R(x[1],(i&1)?24:31),
-    x[3]+=*x,
-    x[3]=R(x[3],(i&1)?31:24),
-    *x=x[1],x[1]=x[2],x[2]=t;
+    t=w[3],w[0]^=i,w[3]=rk[i&7],
+    w[3]^=R(w[1],(i&1)?24:31),
+    w[3]+=w[0],
+    w[3]=R(w[3],(i&1)?31:24),
+    w[0]=w[1],w[1]=w[2],w[2]=t;
 }
